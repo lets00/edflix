061bc3b0232a04d8733c38f240684df1b8049063
Luís Eduardo <eduardovansilva@gmail.com>
2020-07-29 21:07:03 -0300

    Adicionando formulário simples na página de categoria

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -6,6 +6,11 @@ export default function CadstroCategoria() {
     return (
         <PageDefault>
             <h1>Cadastro de categoria</h1>
+            <form>
+                <label>Nome da categoria</label>
+                <input type="text"/>
+                <button type="submit">Cadsatrar</button>
+            </form>
             <Link to='/'>Ir para home</Link>
         </PageDefault>
     )
